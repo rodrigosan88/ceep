5b5b51483b7489c541b251709c876266f036d812
Rodrigo Santos <rodrigo.gremios@gmail.com>
2016-08-03 12:47:17 -0300

    Commit aula 3

--- js/principal.js
+++ js/principal.js
@@ -20,3 +20,39 @@ function removeCartao(){
         cartao.remove();
     }, 400);
 }
+
+var contadorCartoes = $('.cartao').length;
+
+$(".novoCartao").submit( function(event){
+
+    var campoConteudo = $('.novoCartao-conteudo');
+    var conteudo = campoConteudo.val().trim();
+    
+    if(conteudo){
+        contadorCartoes++;
+        
+        var botaoRemover = $('<button>')
+                            .addClass('opcoesDoCartao-opcao')
+                            .addClass('opcoesDoCartao-remove')
+                            .attr('data-ref',contadorCartoes)
+                            .text('Remover')
+                            .click(removeCartao);
+        
+        var opcoes = $('<div>').addClass('opcoesDoCartao').append(botaoRemover);
+        
+        var conteudoTag = $('<p>').addClass('cartao-conteudo').append(conteudo);
+        
+        $('<div>')
+            .attr('id', 'cartao_' + contadorCartoes)
+            .addClass('cartao')
+            .append(opcoes)
+            .append(conteudoTag)
+            .prependTo('.mural').fadeIn();
+    } else {
+        $('.mensagem').remove();
+        $('<span>').addClass('mensagem').addClass('erro').append('TESTE').appendTo('.novoCartao').fadeIn(411);
+    }
+    
+    campoConteudo.val('');
+    event.preventDefault();
+});

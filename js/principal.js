983d7b71fbaa41275176e72b02c6d11da3d42628
Rodrigo Santos <rodrigo.gremios@gmail.com>
2016-08-04 12:35:37 -0300

    Commit aula 4

--- js/principal.js
+++ js/principal.js
@@ -26,7 +26,7 @@ var contadorCartoes = $('.cartao').length;
 $(".novoCartao").submit( function(event){
 
     var campoConteudo = $('.novoCartao-conteudo');
-    var conteudo = campoConteudo.val().trim();
+    var conteudo = campoConteudo.val().trim().replace(/\n/g, "<br>");
     
     if(conteudo){
         contadorCartoes++;
@@ -39,12 +39,13 @@ $(".novoCartao").submit( function(event){
                             .click(removeCartao);
         
         var opcoes = $('<div>').addClass('opcoesDoCartao').append(botaoRemover);
-        
+        var tipoCartao = decideTipoCartao(conteudo);
         var conteudoTag = $('<p>').addClass('cartao-conteudo').append(conteudo);
         
         $('<div>')
             .attr('id', 'cartao_' + contadorCartoes)
             .addClass('cartao')
+            .addClass(tipoCartao)
             .append(opcoes)
             .append(conteudoTag)
             .prependTo('.mural').fadeIn();
@@ -55,4 +56,42 @@ $(".novoCartao").submit( function(event){
     
     campoConteudo.val('');
     event.preventDefault();
+});
+
+
+function decideTipoCartao(conteudo){
+    var quebras = conteudo.split("<br>").length;
+    var totalDeLetras = conteudo.replace(/<br>/g, ' ').length;
+    
+    var ultimoMaior = '';
+    conteudo.replace('/<br>/g', ' ')
+        .split(' ')
+        .forEach(function(palavra){
+            if(palavra.length > ultimoMaior.length){
+                ultimoMaior = palavra;
+            }    
+        });
+    var tamMaior = ultimoMaior.length;
+    var tipoCartao = 'cartao--textoPequeno';
+    if(tamMaior < 9 && quebras < 5 && totalDeLetras < 55){
+        tipoCartao = 'cartao--textoGrande';
+    } else if(tamMaior < 12 && quebras < 6 && totalDeLetras < 75){
+        tipoCartao = 'cartao--textoMedio';
+    }
+    debugger;
+    return tipoCartao;
+}
+
+$('#busca').on('input', function(){
+    var busca = $(this).val().trim();
+    
+    if(busca.length){
+        $('.cartao').hide().filter(function(){
+            var conteudoCartao = $(this).find('.cartao-conteudo').text();
+            var regex = new RegExp(busca, 'i');
+            return conteudoCartao.match(regex);
+        }).show();
+    }else{
+        $('.cartao').show();
+    }
 });
